e90d1d62d7a4c1435659d235765248414b13b973
Anthony M <anthony.moffa@thoughtbot.com>
2020-06-28 15:48:44 -0400

    Introduce Message type and fetch messages for a selected group
    This commit introduces a new Message type to represent a message fetched
    through the GroupMe API.
    
    The Model was updated to represent two states, one where the user is
    viewing the list of groups (ViewingGroups) and the other where they are
    viewing a list of messages for a selected group (ViewingMessages)
    
    There are a few hardcoded values in the message decoder that will be
    replaced in future commits.

--- src/Main.elm
+++ src/Main.elm
@@ -3,22 +3,42 @@ module Main exposing (Model, Msg(..), init, main, update, view)
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (src)
+import Html.Events exposing (onClick)
 import Http
-import Json.Decode as Decode exposing (Decoder, int, list, string, succeed)
-import Json.Decode.Pipeline exposing (required)
+import Json.Decode as Decode
+    exposing
+        ( Decoder
+        , andThen
+        , field
+        , int
+        , list
+        , nullable
+        , oneOf
+        , string
+        , succeed
+        )
+import Json.Decode.Pipeline
+    exposing
+        ( custom
+        , hardcoded
+        , optional
+        , required
+        , requiredAt
+        )
 
 
 
 ---- MODEL ----
 
 
-type alias Model =
-    { groups : List Group }
+type Model
+    = ViewingGroups (List Group)
+    | ViewingMessages String (List Message)
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( { groups = [] }, getGroups )
+    ( ViewingGroups [], getGroups )
 
 
 type alias GroupMeResponse a =
@@ -30,6 +50,24 @@ type alias Group =
     { id : String, name : String }
 
 
+type alias Message =
+    { id : String
+    , createdAt : Int
+    , text : Maybe String
+    , authorName : String
+    , avatarUrl : String
+    , attachments : List Attachment
+    , favoritesCount : Int
+    }
+
+
+type Attachment
+    = Image { url : String }
+    | Location { lat : String, lng : String, name : String }
+    | Split { token : String }
+    | Emoji { placeholder : String, charMap : List (List Int) }
+
+
 
 ---- UPDATE ----
 
@@ -37,6 +75,8 @@ type alias Group =
 type Msg
     = NoOp
     | GotGroups (Result Http.Error (GroupMeResponse (List Group)))
+    | GotMessages String (Result Http.Error (GroupMeResponse (List Message)))
+    | UserSelectedGroup String
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -48,7 +88,7 @@ update msg model =
         GotGroups result ->
             case result of
                 Ok { response } ->
-                    ( { model | groups = response }, Cmd.none )
+                    ( ViewingGroups response, Cmd.none )
 
                 Err error ->
                     let
@@ -57,6 +97,21 @@ update msg model =
                     in
                     ( model, Cmd.none )
 
+        GotMessages groupId result ->
+            case result of
+                Ok { response } ->
+                    ( ViewingMessages groupId response, Cmd.none )
+
+                Err error ->
+                    let
+                        _ =
+                            Debug.log "error" error
+                    in
+                    ( model, Cmd.none )
+
+        UserSelectedGroup groupId ->
+            ( model, getMessages groupId )
+
 
 
 ---- VIEW ----
@@ -64,12 +119,22 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div [] <| List.map viewGroup model.groups
+    case model of
+        ViewingGroups groups ->
+            div [] <| List.map viewGroup groups
+
+        ViewingMessages _ messages ->
+            div [] <| List.map viewMessage messages
 
 
 viewGroup : Group -> Html Msg
 viewGroup group =
-    div [] [ h2 [] [ text group.name ] ]
+    div [] [ h2 [ onClick <| UserSelectedGroup group.id ] [ text group.name ] ]
+
+
+viewMessage : Message -> Html Msg
+viewMessage message =
+    div [] [ text <| Maybe.withDefault "" message.text ]
 
 
 
@@ -83,7 +148,12 @@ baseApiUrl =
 
 groupsUrl : String
 groupsUrl =
-    baseApiUrl ++ "/groups" ++ apiTokenParam
+    baseApiUrl ++ "/groups"
+
+
+messagesUrl : String -> String
+messagesUrl groupId =
+    groupsUrl ++ "/" ++ groupId ++ "/messages"
 
 
 apiTokenParam : String
@@ -91,10 +161,15 @@ apiTokenParam =
     Debug.todo "Add your GroupMe API token here: ?token="
 
 
+urlWithToken : String -> String
+urlWithToken url =
+    url ++ apiTokenParam
+
+
 getGroups : Cmd Msg
 getGroups =
     Http.get
-        { url = groupsUrl
+        { url = urlWithToken groupsUrl
         , expect = Http.expectJson GotGroups groupListDecoder
         }
 
@@ -110,6 +185,84 @@ groupListDecoder : Decoder (GroupMeResponse (List Group))
 groupListDecoder =
     succeed GroupMeResponse
         |> required "response" (list groupDecoder)
+
+
+getMessages : String -> Cmd Msg
+getMessages groupId =
+    Http.get
+        { url = urlWithToken <| messagesUrl groupId
+        , expect = Http.expectJson (GotMessages groupId) messageListDecoder
+        }
+
+
+messageListDecoder : Decoder (GroupMeResponse (List Message))
+messageListDecoder =
+    succeed GroupMeResponse
+        |> requiredAt [ "response", "messages" ] (list messageDecoder)
+
+
+messageDecoder : Decoder Message
+messageDecoder =
+    succeed Message
+        |> required "id" string
+        |> required "created_at" int
+        |> required "text" (nullable string)
+        |> required "name" string
+        |> required "avatar_url" string
+        |> required "attachments" (list attachmentDecoder)
+        |> hardcoded 0
+
+
+attachmentDecoder : Decoder Attachment
+attachmentDecoder =
+    oneOf [ imageDecoder, locationDecoder, splitDecoder, emojiDecoder ]
+
+
+imageDecoder : Decoder Attachment
+imageDecoder =
+    succeed imageFromResponse
+        |> required "url" string
+
+
+locationDecoder : Decoder Attachment
+locationDecoder =
+    succeed locationFromResponse
+        |> required "lat" string
+        |> required "lng" string
+        |> required "name" string
+
+
+splitDecoder : Decoder Attachment
+splitDecoder =
+    succeed splitFromResponse
+        |> required "token" string
+
+
+emojiDecoder : Decoder Attachment
+emojiDecoder =
+    succeed emojiFromResponse
+        |> required "placeholder" string
+        |> hardcoded []
+
+
+imageFromResponse : String -> Attachment
+imageFromResponse url =
+    Image { url = url }
+
+
+locationFromResponse : String -> String -> String -> Attachment
+locationFromResponse lat lng name =
+    Location { lat = lat, lng = lng, name = name }
+
+
+splitFromResponse : String -> Attachment
+splitFromResponse token =
+    Split { token = token }
+
+
+emojiFromResponse : String -> List (List Int) -> Attachment
+emojiFromResponse placeholder charMap =
+    Emoji { placeholder = placeholder, charMap = charMap }
 
 
 
